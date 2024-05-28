17b8f6843eceabd3a7c88af76fc9372c26ccd7a1
Muhammad Ayaz <muhammadayaz22757@gmail.com>
2024-05-28 11:07:33 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React  from "react";
+import Mine from "./Mine";
+
+function App() {
+  return (
+    <div className="App">
+    <Mine />
+      
+  <h1>Hello World</h1>
+  <p>Islamia College</p>
+  <h1>{2+2}</h1>
+    </div>
+    
+    
+  )
+}
+
+export default App;
---
+++ src/Mine.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import theImg from "./ab.jpg"
+function Mine(){
+    return (
+        <div className="xyz">
+            <h1>Name : Muhammad Ayaz</h1>
+            <h1>Class : Web and App Development</h1>
+            <h1>Roll No: 221331</h1>
+            <img src={theImg} width={200} />
+        </div>
+    )
+}
+export default Mine;
+
+
